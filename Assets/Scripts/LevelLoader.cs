8606c650b976ad981a672d7386510cc8ce7eb823
ZacSlater <zachary.slater@student.uts.edu.au>
2023-10-24 12:49:47 +1100

    Player UI and exit button functionality

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -19,10 +19,5 @@ public class LevelLoader : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
-
-    public void LoadStartScreen()
-    {
-        SceneManager.LoadScene(0);
-    }
 }
 
